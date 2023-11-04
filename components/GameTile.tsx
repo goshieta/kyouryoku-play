b1ee35808e35e99c400ab41417e571979b3174b4
goshieta <tamerika563@gmail.com>
2023-11-04 16:41:51 +0900

    ホーム画面実装完了

--- components/GameTile.tsx
+++ components/GameTile.tsx
@@ -1,14 +1,59 @@
 import styles from "@/styles/components/gameTile.module.css";
 
 type gameTilePropsType = {
-  themeColor: String;
-  gameTitle: String;
-  gamePhrase?: String;
-  gameDesc?: String;
-  link: String;
-  imgLink?: String;
+  themeColor: string;
+  gameTitle: string;
+  size: "big" | "small";
+  gamePhrase?: string;
+  gameDesc?: string;
+  link: string;
+  imgLink?: string;
 };
 
 export default function GameTile(props: gameTilePropsType) {
-  return <div id={styles.tile}></div>;
+  return (
+    <div
+      id={styles.tile}
+      style={{
+        backgroundColor: props.themeColor,
+        ...(props.size == "big"
+          ? {
+              gridColumnStart: 1,
+              gridColumnEnd: 3,
+            }
+          : {}),
+      }}
+    >
+      <div id={styles.top}>
+        <div id={styles.left}>
+          {props.size == "big" ? <h2>{props.gamePhrase}</h2> : <></>}
+          <h3>{props.gameTitle}</h3>
+          {props.size == "big" ? <p>{props.gameDesc}</p> : <></>}
+          {props.size == "small" ? <a href={props.link}>プレイ ＞</a> : <></>}
+        </div>
+        <div
+          id={styles.right}
+          className={props.size == "big" ? styles.big : ""}
+        >
+          {props.imgLink != undefined ? (
+            <img
+              src={props.imgLink}
+              alt={props.gameTitle}
+              width={150}
+              height={150}
+            />
+          ) : (
+            <div></div>
+          )}
+        </div>
+      </div>
+      {props.size == "big" ? (
+        <div id={styles.bottom}>
+          <a href={props.link}>プレイ ＞</a>
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -12,13 +12,38 @@ export default function Home() {
           content="使いやすく、シンプル、現実世界を重視した新世代のゲームサイト「峡緑プレイ」へようこそ。このサイトでは軽いボードゲームを中心に、様々なゲームを楽しめます。"
         />
       </Head>
-      <div id="gridGameArea">
+      <div id={styles.gridGameArea}>
         <GameTile
           gameTitle="オセロ(リバーシ)"
-          themeColor="#00FFA3"
+          themeColor="#00ffa2"
           link="/"
+          size="big"
           gamePhrase="定番こそ、今やるべき"
           gameDesc="ボードゲームの中でも定番中の定番である「オセロ（リバーシ）」。定番だからこそ、久しぶりにやってみると奥が深いものです。"
+        ></GameTile>
+        <GameTile
+          gameTitle="将棋"
+          themeColor="#b994ff"
+          link="/"
+          size="small"
+        ></GameTile>
+        <GameTile
+          gameTitle="囲碁"
+          themeColor="#b994ff"
+          link="/"
+          size="small"
+        ></GameTile>
+        <GameTile
+          gameTitle="コネクト4"
+          themeColor="#b994ff"
+          link="/"
+          size="small"
+        ></GameTile>
+        <GameTile
+          gameTitle="フラッシュ暗算"
+          themeColor="#b994ff"
+          link="/"
+          size="small"
         ></GameTile>
       </div>
     </div>
