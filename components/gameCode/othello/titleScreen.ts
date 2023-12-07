90f023f7cf597f51b24626171ad404c5be5e6a0c
goshieta <tamerika563@gmail.com>
2023-12-07 16:44:30 +0900

    シングルプレイのみに変更

--- components/gameCode/othello/titleScreen.ts
+++ components/gameCode/othello/titleScreen.ts
@@ -17,18 +17,17 @@ export class titleScreen extends Phaser.Scene {
 
     const buttonDom = this.add.dom(250, 300).createFromHTML(`
     <div>
-      <button id="singlePlayButton" style="cursor:pointer;margin:60px;display:block;border-radius:5px;width:200px;height:60px;font-size:20px;font-weight:bold;">一人で対戦</button>
-      <button id="doublePlayButton" style="cursor:pointer;margin:60px;display:block;border-radius:5px;width:200px;height:60px;font-size:20px;font-weight:bold;">二人で対戦</button>
+      <button id="singlePlayButton" style="cursor:pointer;margin:60px;display:block;border-radius:5px;width:200px;height:60px;font-size:20px;font-weight:bold;">始める</button>
     </div>`);
     buttonDom
       .getChildByID("singlePlayButton")
       ?.addEventListener("click", () => {
         this.scene.start("game", { mode: "bot" });
       });
-    buttonDom
+    /*buttonDom
       .getChildByID("doublePlayButton")
       ?.addEventListener("click", () => {
         this.scene.start("game", { mode: "player" });
-      });
+      });*/
   }
 }
