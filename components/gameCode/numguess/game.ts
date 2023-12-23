13cbf7647854237671502301d5f4d08643302635
goshieta <tamerika563@gmail.com>
2023-12-23 19:08:10 +0900

    numguess:解析メソッドを実装

--- components/gameCode/numguess/game.ts
+++ components/gameCode/numguess/game.ts
@@ -1,15 +1,30 @@
 export class game extends Phaser.Scene {
   level: number;
   correctNumber: number;
+  seconds: number;
+  times: number;
+
+  private domItem?: {
+    seconds: Element;
+    times: Element;
+    input: Element;
+    submitButton: Element;
+  };
+
   constructor() {
     super({ key: "game" });
 
     this.level = 0;
     this.correctNumber = 0;
+    this.seconds = 0;
+    this.times = 0;
   }
 
   init(config: { level: number }) {
     this.level = config.level;
+
+    this.seconds = 100 + this.level * 50;
+    this.times = 10 + this.level * 5;
   }
 
   create() {
@@ -31,18 +46,18 @@ export class game extends Phaser.Scene {
         <div id="ng_input_area">
           <h2>${["初級", "中級", "上級"][this.level]}</h2>
           <div>
-            <input type="number"></input><button>回答</button>
+            <input type="number" id="ng_input"></input><button id="ng_submit">回答</button>
           </div>
         </div>
         <div id="ng_history"></div>
         <div id="ng_para">
           <div id="ng_seconds">
             <p>残り時間</p>
-            <p>0.0</p>
+            <p id="ng_seconds_p">${this.seconds}</p>
           </div>
           <div id="ng_times">
             <p>残り回数</p>
-            <p>0</p>
+            <p id="ng_times_p">${this.times}</p>
           </div>
         </div>
       </div>
@@ -99,5 +114,79 @@ export class game extends Phaser.Scene {
       </style>
     `;
     const gameDom = this.add.dom(250, 250).createFromHTML(gameCode);
+
+    this.domItem = {
+      seconds: <Element>gameDom.getChildByID("ng_seconds_p"),
+      times: <Element>gameDom.getChildByID("ng_times_p"),
+      input: <Element>gameDom.getChildByID("ng_input"),
+      submitButton: <Element>gameDom.getChildByID("ng_submit"),
+    };
+
+    //イベントを設定
+    this.domItem.submitButton.addEventListener("click", () => {
+      if (this.domItem === undefined) return;
+      const result = this.parse(
+        this.domItem.input.innerHTML,
+        this.correctNumber
+      );
+      console.log(result);
+    });
+  }
+
+  update(time: number, delta: number): void {
+    this.seconds -= delta / 1000;
+
+    if (this.domItem === undefined) return;
+    this.domItem.seconds.innerHTML = String(Math.floor(this.seconds));
+    this.domItem.times.innerHTML = String(this.times);
+  }
+
+  //入力された数字を答えと照らし合わせる
+  parse(userAnswer: string, correctNumber: number) {
+    const stringCorrectNumber = String(correctNumber);
+
+    //例外処理
+    const exceptionArray = [
+      {
+        regularExpression: `^.{${String(this.level + 3)}}$`,
+        errorMessage: "桁数が違います",
+      },
+      {
+        regularExpression: /^[0-9]+$/,
+        errorMessage: "数字以外が含まれています",
+      },
+      {
+        regularExpression: /^(?!.*(.).*\1)[^$]+$/,
+        errorMessage: "重複した数字が存在します",
+      },
+    ];
+    const errorMes: string[] = [];
+    exceptionArray.forEach((oneException) => {
+      const regularExpression = new RegExp(oneException.regularExpression);
+      const isOk = regularExpression.test(userAnswer);
+      if (!isOk) errorMes.push(oneException.errorMessage);
+    });
+    if (errorMes.length === 0)
+      return { errorMes: errorMes, round: 0, triangle: 0 };
+
+    //マッチしている数を調べる
+    const roundNumber = Array.from(stringCorrectNumber).reduce(
+      (previousValue, currentValue, currentIndex) => {
+        if (userAnswer[currentIndex] === currentValue)
+          return String(Number(previousValue) + 1);
+        else return previousValue;
+      }
+    );
+    const triangleNumber = Array.from(stringCorrectNumber).reduce(
+      (previousValue, currentValue, currentIndex) => {
+        const isMatch = Array.from(userAnswer).some(
+          (val, index) => val === currentValue && index !== currentIndex
+        );
+        if (isMatch) return String(Number(previousValue) + 1);
+        else return previousValue;
+      }
+    );
+
+    return { errorMes: errorMes, round: roundNumber, triangle: triangleNumber };
   }
 }
