d34c44499dba6d7545fa555ff328ea7063d565ab
goshieta <tamerika563@gmail.com>
2024-01-24 15:09:25 +0900

    Google関連追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,30 @@
-import { AppProps } from 'next/app'
-import Head from 'next/head'
-import Footer from '@/components/Footer'
-import Header from '@/components/Header'
-import '@/styles/globals.css'
+import { AppProps } from "next/app";
+import Head from "next/head";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import "@/styles/globals.css";
+import Script from "next/script";
 
 const App = ({ Component, pageProps }: AppProps) => (
   <>
     <Head>
       <title>峡緑プレイ | KyouRyoku Play</title>
-      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-CJT63ZRL0E"
+      ></Script>
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-CJT63ZRL0E');
+      `,
+        }}
+      ></Script>
     </Head>
     <div id="rootParent">
       <div id="noneFooterContent">
@@ -20,6 +36,6 @@ const App = ({ Component, pageProps }: AppProps) => (
       <Footer></Footer>
     </div>
   </>
-)
+);
 
-export default App
+export default App;
