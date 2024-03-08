0bdb733c8bba04abc0e620f6f97127542c3ecfed
goshieta <tamerika563@gmail.com>
2024-03-08 17:20:50 +0900

    アカウントページの基本のレイアウト作成

--- pages/account/index.tsx
+++ pages/account/index.tsx
@@ -1,16 +1,31 @@
 import { useAuth } from "@/components/context/auth";
-import { logout } from "@/lib/auth";
+import { login, logout } from "@/lib/auth";
+import Image from "next/image";
+import styles from "@/styles/pages/account.module.css";
 
 export default function Account() {
+  const authInfo = useAuth();
+  if (!authInfo) {
+    return <button onClick={login}>ログイン</button>;
+  }
+
   return (
-    <>
-      <div>
-        <p>ID : {useAuth()?.id}</p>
-        <p>名前 : {useAuth()?.name}</p>
-        <p>メールアドレス : {useAuth()?.email}</p>
-        <img src={useAuth()?.photoURL} alt="" />
+    <div id={styles.accountPage}>
+      <div id={styles.accountInfo}>
+        <div id={styles.ai_image_area}>
+          <Image
+            src={authInfo.photoURL}
+            alt="プロフィール画像"
+            width={100}
+            height={100}
+          ></Image>
+        </div>
+        <div id={styles.ai_desc_area}>
+          <h3>{authInfo.name}</h3>
+          <p>{authInfo.email}</p>
+        </div>
       </div>
       <button onClick={logout}>ログアウト</button>
-    </>
+    </div>
   );
 }
