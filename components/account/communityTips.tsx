20f91655f41f7f949e6c6331a07aee42e5bb0a38
goshieta <tamerika563@gmail.com>
2024-03-28 14:21:46 +0900

    アカウントページのコミュニティ表示を作成

--- components/account/communityTips.tsx
+++ components/account/communityTips.tsx
@@ -6,7 +6,7 @@ import {
 import styles from "@/styles/pages/account.module.css";
 import { useEffect, useState } from "react";
 import OneCommunityTip from "./oneCommunityTip";
-import { doc, getDoc } from "firebase/firestore";
+import { arrayRemove, doc, getDoc, updateDoc } from "firebase/firestore";
 import { db } from "@/lib/firebase/client";
 
 export default function CommunityTip({ authInfo }: { authInfo: userType }) {
@@ -21,19 +21,31 @@ export default function CommunityTip({ authInfo }: { authInfo: userType }) {
     );
     Promise.all(communityInfoPromise).then((communitiesDoc) => {
       const newCommunities: communityType[] = [];
+      const invalidCommunities: string[] = [];
       communitiesDoc.forEach((oneCommunity) => {
         const data = oneCommunity.data();
         if (isCommunityType(data)) newCommunities.push(data);
+        else invalidCommunities.push(oneCommunity.id);
       });
       setCommunities(newCommunities);
+      updateDoc(doc(db, "users", authInfo.id), {
+        belongCommunity: arrayRemove(...invalidCommunities),
+      });
     });
   }, []);
 
   return (
-    <div id={styles.communityTip}>
-      {communities?.map((oneCommunity) => (
-        <OneCommunityTip communityInfo={oneCommunity} key={oneCommunity.id} />
-      ))}
+    <div>
+      <h2 id={styles.communityAreaTitle}>所属しているコミュニティ</h2>
+      <div id={styles.communityTip}>
+        {communities?.map((oneCommunity) => (
+          <OneCommunityTip
+            communityInfo={oneCommunity}
+            key={oneCommunity.id}
+            userInfo={authInfo}
+          />
+        ))}
+      </div>
     </div>
   );
 }
