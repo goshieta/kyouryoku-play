2868c09387a1358600f7f7904abd8684798757fa
goshieta <tamerika563@gmail.com>
2024-04-05 13:26:01 +0900

    投稿がちゃんと表示されるようんい

--- components/world/top/articles.tsx
+++ components/world/top/articles.tsx
@@ -1,17 +1,24 @@
 import { db } from "@/lib/firebase/client";
-import { isOneArticleType, oneArticleType } from "@/lib/types/communityType";
+import {
+  isOneArticleType,
+  isPubUserDataType,
+  oneArticleType,
+  pubUserDataType,
+} from "@/lib/types/communityType";
 import styles from "@/styles/world/world.module.css";
 import {
   QueryDocumentSnapshot,
   collection,
   getDocs,
+  getDoc,
+  doc,
   limit,
   orderBy,
   query,
   where,
 } from "firebase/firestore";
 import { useEffect, useState } from "react";
-import OneArticle from "./oneArticle";
+import OneArticle, { getOneUserInfo } from "./oneArticle";
 import { DocumentData } from "firebase-admin/firestore";
 
 export default function Articles({ query }: { query: string | null }) {
@@ -19,13 +26,15 @@ export default function Articles({ query }: { query: string | null }) {
   const [artSnapshots, setArtsSnapShots] = useState<
     QueryDocumentSnapshot<DocumentData, DocumentData>[]
   >([]);
+  const [usersInfo, setUsersInfo] = useState<
+    { [key: string]: pubUserDataType } | undefined
+  >(undefined);
 
   useEffect(() => {
     //記事を取得
     const queryEffect = async () => {
       console.log(query);
       const articles = await getArticleFromQuery(query);
-      console.log(articles);
       setArtsSnapShots(articles);
     };
     queryEffect();
@@ -40,11 +49,39 @@ export default function Articles({ query }: { query: string | null }) {
     setArts(newArts);
   }, [artSnapshots]);
 
+  //通信量を抑えるためにここでユーザーデータを最初に取得する
+  useEffect(() => {
+    const getUsersInfo = async () => {
+      if (!arts) return;
+      if (!usersInfo) {
+        let newUsers: { [key: string]: pubUserDataType } = {};
+        for (const oneMessage of arts) {
+          const localUserInfo = newUsers[oneMessage.user];
+          if (localUserInfo === undefined) {
+            const user = await getOneUserInfo(oneMessage.user);
+            if (user) newUsers = { ...newUsers, [oneMessage.user]: user };
+          }
+        }
+        setUsersInfo(newUsers);
+      }
+    };
+    getUsersInfo();
+  }, [arts]);
+
   return (
     <div id={styles.articles}>
-      {arts?.map((oneArts) => (
-        <OneArticle article={oneArts} key={oneArts.id}></OneArticle>
-      ))}
+      {usersInfo ? (
+        arts?.map((oneArts) => (
+          <OneArticle
+            article={oneArts}
+            key={oneArts.id}
+            usersInfo={usersInfo}
+            setUsersInfo={setUsersInfo}
+          ></OneArticle>
+        ))
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
--- components/world/top/oneArticle.tsx
+++ components/world/top/oneArticle.tsx
@@ -1,9 +1,72 @@
-import { oneArticleType } from "@/lib/types/communityType";
+import { db } from "@/lib/firebase/client";
+import {
+  isPubUserDataType,
+  oneArticleType,
+  pubUserDataType,
+} from "@/lib/types/communityType";
+import styles from "@/styles/world/world.module.css";
+import { doc, getDoc } from "firebase/firestore";
+import Image from "next/image";
+import { useEffect, useMemo, useState } from "react";
 
-export default function OneArticle({ article }: { article: oneArticleType }) {
+export async function getOneUserInfo(id: string) {
+  const document = await getDoc(doc(db, "pubUsers", id));
+  const data = document.data();
+  if (isPubUserDataType(data)) return data;
+  else return undefined;
+}
+
+export default function OneArticle({
+  article,
+  usersInfo,
+  setUsersInfo,
+}: {
+  article: oneArticleType;
+  usersInfo: { [key: string]: pubUserDataType };
+  setUsersInfo: (newUsersInfo: { [key: string]: pubUserDataType }) => void;
+}) {
+  //ユーザーのデータを設定
+  const [userData, setUserData] = useState<pubUserDataType | null | undefined>(
+    usersInfo[article.user] ? usersInfo[article.user] : null
+  );
+  useEffect(() => {
+    if (userData === null) {
+      getOneUserInfo(article.user).then((data) => {
+        setUserData(data);
+        if (data) {
+          setUsersInfo({ ...usersInfo, [article.user]: data });
+        }
+        return true;
+      });
+    }
+  }, [usersInfo]);
+  const createdAt = useMemo(() => new Date(article.createdAt), []);
+  const zeroPadding = (digit: number, num: number) =>
+    ("0".repeat(digit) + num.toString()).slice(-digit);
+
   return (
-    <div>
+    <div className={styles.oneArticle}>
+      {userData ? (
+        <div className={styles.accountArea}>
+          <Image
+            src={userData.photoURL}
+            width={30}
+            height={30}
+            alt={userData.name}
+          />
+          <p>{userData.name}</p>
+          <p>{`${createdAt.getFullYear()}年${
+            createdAt.getMonth() + 1
+          }月${createdAt.getDate()}日 ${zeroPadding(
+            2,
+            createdAt.getHours()
+          )}:${zeroPadding(2, createdAt.getMinutes())}`}</p>
+        </div>
+      ) : (
+        <></>
+      )}
       <h3>{article.title}</h3>
+      <p>{article.description}</p>
     </div>
   );
 }
