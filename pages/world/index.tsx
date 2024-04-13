e2b61c037fa7b1e31b58900415c732ea8d28a0be
goshieta <tamerika563@gmail.com>
2024-04-13 16:59:48 +0900

    タグ機能とページの結びつきがUIにおいてより一層強く

--- pages/world/index.tsx
+++ pages/world/index.tsx
@@ -22,7 +22,7 @@ export default function World() {
       .then((data) => data.text())
       .then((txt) => {
         const trend = JSON.parse(txt);
-        trend.length = 10;
+        trend.length = 5;
         setTrendingTag(trend);
       });
   }, []);
@@ -31,15 +31,36 @@ export default function World() {
   return (
     <div id={styles.parent}>
       <Head>
-        <title>{`${currentTag}の投稿 - 峡緑プレイ`}</title>
+        <title>{`${
+          !currentTag?.currentTag ? "すべて" : currentTag?.currentTag
+        }の投稿 - 峡緑プレイ`}</title>
       </Head>
       <div id={styles.leftItem}>
-        <button id={styles.createNew} onClick={() => router.push("/world/new")}>
+        <button
+          id={styles.createNew}
+          className={styles.leftButton}
+          onClick={() => router.push("/world/new")}
+        >
           <span className="material-symbols-outlined">article</span>投稿
         </button>
-        <div id={styles.trend}>
+        <div id={styles.linkArea}>
+          <Link
+            id={styles.all}
+            href={`/world?q=すべて`}
+            className={
+              currentTag?.currentTag === "すべて" ? styles.tagCurrent : ""
+            }
+          >
+            <span className="material-symbols-outlined">home</span>すべて
+          </Link>
           {trendingTag.map((oneTag) => (
-            <Link key={oneTag} href={`/world?q=${oneTag}`}>
+            <Link
+              key={oneTag}
+              href={`/world?q=${oneTag}`}
+              className={
+                currentTag?.currentTag === oneTag ? styles.tagCurrent : ""
+              }
+            >
               <span className="material-symbols-outlined">trending_up</span>
               {oneTag}
             </Link>
