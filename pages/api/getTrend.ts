a3fc8878d1f3ffe029eda52d0b02b7f3068ed588
goshieta <tamerika563@gmail.com>
2024-04-13 19:57:15 +0900

    APIの問題を解決

--- pages/api/getTrend.ts
+++ pages/api/getTrend.ts
@@ -3,11 +3,13 @@ import jsdom from "jsdom";
 
 type Data = string[];
 
-export default function getTrend(
+export default async function getTrend(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  fetch("https://trends.google.com/trends/trendingsearches/daily/rss?geo=JP")
+  return await fetch(
+    "https://trends.google.com/trends/trendingsearches/daily/rss?geo=JP"
+  )
     .then((data) => data.text())
     .then((txt) => {
       const doc = new jsdom.JSDOM(txt);
@@ -15,5 +17,6 @@ export default function getTrend(
         doc.window.document.querySelectorAll("item > title")
       ).map((oneElement) => oneElement.innerHTML);
       res.status(200).json(trends);
+      return;
     });
 }
