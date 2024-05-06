f04fab2123f7b6e2a06bda6361867964d0fbb30e
goshieta <tamerika563@gmail.com>
2024-05-06 17:47:43 +0900

    ゲームのトップページ作成

--- pages/games/index.tsx
+++ pages/games/index.tsx
@@ -1,5 +1,28 @@
-import Menu from "@/components/top/menu";
+import GameTile from "@/components/GameTile";
+import Head from "next/head";
+import styles from "@/styles/pages/gameTop.module.css";
 
 export default function GameTop() {
-  return <div></div>;
+  const games = [
+    "soccer",
+    "westeastbuttle",
+    "castlerun",
+    "numguess",
+    "flyfly",
+    "flash",
+    "othello",
+  ];
+  return (
+    <div id={styles.top}>
+      <Head>
+        <title>すべてのゲーム - 峡緑プレイ</title>
+      </Head>
+      <h1>すべてのゲーム</h1>
+      <div id={styles.games}>
+        {games.map((gameName) => (
+          <GameTile gameCode={gameName} />
+        ))}
+      </div>
+    </div>
+  );
 }
