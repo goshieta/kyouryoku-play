3d92f19bd1ebe82aa3a4c59d1cc27fa9bc69fa62
goshieta <tamerika563@gmail.com>
2024-05-19 15:04:02 +0900

    検索のサジェストを追加

---
+++ components/world/common/suggestTag.tsx
@@ -0,0 +1,49 @@
+import { db } from "@/lib/firebase/client";
+import createNGrams from "@/lib/tips/createNgram";
+import { isTagType } from "@/lib/types/communityType";
+import { collection, getDocs, limit, where, query } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import styles from "@/styles/world/world.module.css";
+
+export default function SuggestTag({
+  strQuery,
+  setQuery,
+}: {
+  strQuery: string;
+  setQuery: (newQuery: string) => void;
+}) {
+  const [suggests, setSuggests] = useState<string[]>([]);
+  useEffect(() => {
+    if (strQuery.length < 2) {
+      setSuggests([]);
+      return;
+    }
+    const ngrams = createNGrams(strQuery, 2);
+    const ngramQuery = [];
+    for (let onestr in ngrams) {
+      ngramQuery.push(where(`forSearch.${onestr}`, "==", true));
+    }
+    getDocs(query(collection(db, "tags"), limit(5), ...ngramQuery)).then(
+      (someData) => {
+        const tags: string[] = [];
+        someData.forEach((data) => {
+          const oneData = data.data();
+          if (isTagType(oneData)) {
+            tags.push(oneData.name);
+          }
+        });
+        setSuggests(tags);
+      }
+    );
+  }, [strQuery]);
+
+  return (
+    <div id={styles.suggests}>
+      {suggests.map((suggest) => (
+        <p key={suggest} onClick={() => setQuery(suggest)}>
+          {suggest}
+        </p>
+      ))}
+    </div>
+  );
+}
--- components/world/top/query.tsx
+++ components/world/top/query.tsx
@@ -2,6 +2,7 @@ import { CurrentTagContext } from "@/components/layouts/worldLayout";
 import styles from "@/styles/world/world.module.css";
 import { useRouter } from "next/router";
 import { useContext, useState } from "react";
+import SuggestTag from "../common/suggestTag";
 
 export default function Query() {
   const currentTag = useContext(CurrentTagContext);
@@ -12,27 +13,38 @@ export default function Query() {
   );
   const router = useRouter();
 
-  const handleSearch = () => {
+  const handleSearch = (value?: string) => {
+    if (!value) value = currentInputTag;
     if (router.route === "/world") {
-      currentTag?.setCurrentTag(currentInputTag);
+      currentTag?.setCurrentTag(value);
     } else {
-      router.push(`/world?q=${currentInputTag}`);
+      router.push(`/world?q=${value}`);
     }
   };
 
   return (
     <div id={styles.tags}>
       <div id={styles.search}>
-        <input
-          type="text"
-          placeholder="タグ名を入力"
-          value={currentInputTag}
-          onChange={(e) => setCurrentInputTag(e.target.value)}
-          onKeyDown={(e) => {
-            if (!e.nativeEvent.isComposing && e.key === "Enter") handleSearch();
-          }}
-        />
-        <button onClick={handleSearch}>
+        <div id={styles.serachInput}>
+          <input
+            type="text"
+            placeholder="タグ名を入力"
+            value={currentInputTag}
+            onChange={(e) => setCurrentInputTag(e.target.value)}
+            onKeyDown={(e) => {
+              if (!e.nativeEvent.isComposing && e.key === "Enter")
+                handleSearch();
+            }}
+          />
+          <SuggestTag
+            strQuery={currentInputTag}
+            setQuery={(newq) => {
+              setCurrentInputTag(newq);
+              handleSearch(newq);
+            }}
+          />
+        </div>
+        <button onClick={() => handleSearch()}>
           <span className="material-symbols-outlined">search</span>
         </button>
       </div>
