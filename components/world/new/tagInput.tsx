8834d836c077b18594c6388c7f797149aeb179b3
goshieta <tamerika563@gmail.com>
2024-05-25 20:38:34 +0900

    タグの入力をいい感じに行えるように

--- components/world/new/tagInput.tsx
+++ components/world/new/tagInput.tsx
@@ -1,5 +1,5 @@
 import styles from "@/styles/world/new/common.module.css";
-import { useState } from "react";
+import { ChangeEvent, MouseEvent, useCallback, useRef, useState } from "react";
 import SuggestTag from "../common/suggestTag";
 
 export default function TagInput({
@@ -10,17 +10,59 @@ export default function TagInput({
   setTags: (newVal: string[]) => void;
 }) {
   const [currentInputTag, setCurrentInputTag] = useState("");
+  const tagInput = useRef<HTMLInputElement>(null);
+
+  const handleChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      const val = e.target.value;
+      //スペースを含んでいるかで処理を変える
+      if (
+        (val.slice(-1) === " " || val.slice(-1) === "　") &&
+        val.trim() !== ""
+      ) {
+        //スペースを含んでいる
+        setTags([...tags, val.trim()]);
+        setCurrentInputTag("");
+      } else {
+        setCurrentInputTag(val.trim());
+      }
+    },
+    [currentInputTag, tags]
+  );
+  const handleDeleteTag = useCallback(
+    (targetVal: string) => {
+      const value = targetVal.trim();
+      setTags(tags.filter((oneTag) => oneTag !== value));
+    },
+    [tags]
+  );
 
   return (
     <div id={styles.tagInputArea}>
-      <input
-        type="text"
-        placeholder="タグをスペース区切りで入力"
-        id={styles.tagInput}
-        value={currentInputTag}
-        onChange={(e) => setCurrentInputTag(e.target.value)}
+      <div id={styles.tagInput}>
+        {tags.map((oneTag) => (
+          <p className={styles.oneTag} key={oneTag}>
+            {oneTag}
+            <button onClick={() => handleDeleteTag(oneTag)}>×</button>
+          </p>
+        ))}
+        <input
+          type="text"
+          placeholder="タグをスペース区切りで入力"
+          value={currentInputTag}
+          onChange={handleChange}
+          id={styles.tagInputInput}
+          ref={tagInput}
+        />
+      </div>
+      <SuggestTag
+        strQuery={currentInputTag}
+        setQuery={(val) => {
+          setTags([...tags, val]);
+          setCurrentInputTag("");
+          tagInput.current?.focus();
+        }}
       />
-      <SuggestTag strQuery={currentInputTag} setQuery={() => {}} />
     </div>
   );
 }
