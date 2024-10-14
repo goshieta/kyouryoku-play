6ad761de4b8f789ace37e441537f206551ae6dc6
goshiEta <tamerika563@gmail.com>
2024-10-14 10:05:04 +0900

    ゲーム画像を表示させるように

--- app/component/screen.tsx
+++ app/component/screen.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import styles from "@/app/style/page/game.module.css";
+import { useState } from "react";
 
 export default function GameScreen({
   id,
@@ -9,14 +12,28 @@ export default function GameScreen({
   width: number;
   height: number;
 }) {
+  const [isberow, setIsberow] = useState(false);
+
   return (
     <div id={styles.game_screen}>
+      <div
+        id={styles.start_screen}
+        style={{
+          display: isberow ? "none" : "flex",
+          width: width,
+          height: height,
+        }}
+      >
+        <img src={`/api/games/${id}/promote.webp`} alt={`${id}の画像`} />
+        <button onClick={() => setIsberow(true)}>クリックでプレイ</button>
+      </div>
       <iframe
         src={`/api/games/${id}/index.html`}
         loading="lazy"
         width={width}
         height={height}
         id={styles.screen_iframe}
+        style={{ display: isberow ? "block" : "none" }}
       ></iframe>
     </div>
   );
