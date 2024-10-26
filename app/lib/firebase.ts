85a60cba7bc6ed2fd88c433530f3cb5e779df7df
goshiEta <tamerika563@gmail.com>
2024-10-26 22:43:20 +0900

    firebaseのエラーを解決

--- app/lib/firebase.ts
+++ app/lib/firebase.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,5 +22,4 @@ if (!getApps()?.length) {
 }
 
 // Initialize Firebase
-export const analytics = getAnalytics();
 export const db = getFirestore();
