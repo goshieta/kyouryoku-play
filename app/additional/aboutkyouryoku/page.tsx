7f1bdecbd90e3ea154169c57cb64004dcf75f61c
goshiEta <tamerika563@gmail.com>
2024-10-27 13:39:13 +0900

    峡緑についてのページを更新

--- app/additional/aboutkyouryoku/page.tsx
+++ app/additional/aboutkyouryoku/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
-import Link from "next/link";
 import styles from "../additional.module.css";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "峡緑について - 峡緑プレイ",
@@ -10,25 +10,23 @@ export default function Aboutkyouryoku() {
   return (
     <div id={styles.explain_area}>
       <h1>峡緑について</h1>
-      <p>峡緑は、私の作成したサイトにつけているブランド名の一つです。</p>
-      <p>峡緑ブランドのついたサイトは他にも以下のようなサイトがあります。</p>
+      <p>
+        峡緑（KyouRyoku）はウェブサービスを中心に展開する個人開発のブランドです。
+      </p>
+      <p>峡緑の理念は以下のとおりとなっています。</p>
+      <div className={styles.block}>幸せな ”つながり” を実現する</div>
+      <p>
+        峡緑はこの理念のもとに、様々な形のサービスで人と人のつながりを良い形で増幅させることをミッションとします。
+      </p>
+      <p>以下はこのブランドのもとで現在公開されているサービスの一覧です。</p>
       <ul>
         <li>
-          <Link href="/">峡緑プレイ（KyouRyoku Play）</Link>
-        </li>
-        <li>
-          <Link href="https://iyqzbugtwfgzyugaybuxxa.on.drv.tw/Indorde%20top/InT.html">
-            Inforde Top
-          </Link>
-        </li>
-        <li>
-          <Link href="https://iyqzbugtwfgzyugaybuxxa.on.drv.tw/Indorde%20top/InT/Ing/Ing.html">
-            Inforde Game
-          </Link>
-        </li>
-        <li>
-          <Link href="https://iyqzbugtwfgzyugaybuxxa.on.drv.tw/Indorde%20top/InT/%E7%B0%A1%E5%8D%98!%E8%AA%B0%E3%81%A7%E3%82%82!Javascript%E5%85%A5%E9%96%80/%E5%85%A5%E9%96%80.html">
-            簡単！JavaScript入門
+          <Link
+            href="https://kyouryoku-play.vercel.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            峡緑プレイ | KyouRyoku Play
           </Link>
         </li>
       </ul>
