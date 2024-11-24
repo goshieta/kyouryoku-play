fd0677fc1f965f6768c2fc124651a74763d91368
goshiEta <tamerika563@gmail.com>
2024-11-24 14:54:15 +0900

    ユーザーを登録するように

--- app/lib/auth/auth.ts
+++ app/lib/auth/auth.ts
@@ -5,13 +5,14 @@ import {
   OAuthProvider,
 } from "firebase/auth";
 import { auth } from "@/app/lib/firebase"; // Firebaseの初期化を行ったファイルをインポート
+import { registerUser } from "./registerUser";
 
 // Googleでのログイン関数
-export const loginWithGoogle = async (): Promise<UserCredential> => {
+export const loginWithGoogle = async () => {
   const provider = new GoogleAuthProvider(); // Googleの認証プロバイダーを作成
   try {
     const result = await signInWithPopup(auth, provider); // ポップアップでログインを実行
-    return result; // 成功した場合、User  Credentialを返す
+    registerUser(result);
   } catch (error) {
     console.error("Googleログインエラー:", error); // エラー処理
     throw error; // エラーを再スロー
@@ -19,11 +20,11 @@ export const loginWithGoogle = async (): Promise<UserCredential> => {
 };
 
 // Microsoftでのログイン関数
-export const loginWithMicrosoft = async (): Promise<UserCredential> => {
+export const loginWithMicrosoft = async () => {
   const provider = new OAuthProvider("microsoft.com"); // Microsoftの認証プロバイダーを作成
   try {
     const result = await signInWithPopup(auth, provider); // ポップアップでログインを実行
-    return result; // 成功した場合、User  Credentialを返す
+    registerUser(result);
   } catch (error) {
     console.error("Microsoftログインエラー:", error); // エラー処理
     throw error; // エラーを再スロー
