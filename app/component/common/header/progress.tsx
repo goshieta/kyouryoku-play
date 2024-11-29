b306470dcf209dc25b860c6e0d7b46193b18150a
goshiEta <tamerika563@gmail.com>
2024-11-30 07:17:24 +0900

    アカウントバッジを微調整

--- app/component/common/header/progress.tsx
+++ app/component/common/header/progress.tsx
@@ -1,5 +1,5 @@
 const CircularProgressBar = ({ percentage }: { percentage: number }) => {
-  const radius = 20; // 半径を変更（44pxのサイズに合わせるため）
+  const radius = 21; // 半径を変更（44pxのサイズに合わせるため）
   const strokeWidth = 4; // プログレスバーの太さを変更
   const normalizedRadius = radius - strokeWidth * 0.5; // 正規化された半径
   const circumference = normalizedRadius * 2 * Math.PI; // 円周
@@ -8,24 +8,24 @@ const CircularProgressBar = ({ percentage }: { percentage: number }) => {
   const strokeDashoffset = circumference - (percentage / 100) * circumference;
 
   return (
-    <svg width="44" height="44" style={{ transform: "rotate(-90deg)" }}>
+    <svg width="46" height="46" style={{ transform: "rotate(-90deg)" }}>
       {/* 背景の円 */}
       <circle
-        stroke="#e6e6e6" // 背景の色
+        stroke="var(--special-bg)" // 背景の色
         fill="transparent"
         strokeWidth={strokeWidth}
         r={normalizedRadius}
-        cx="22" // 中心のX座標を変更（44pxのサイズに合わせるため）
-        cy="22" // 中心のY座標を変更（44pxのサイズに合わせるため）
+        cx="23" // 中心のX座標を変更（44pxのサイズに合わせるため）
+        cy="23" // 中心のY座標を変更（44pxのサイズに合わせるため）
       />
       {/* プログレスの円 */}
       <circle
-        stroke="#5773ff" // プログレスバーの色
+        stroke="var(--link-color)" // プログレスバーの色
         fill="transparent"
         strokeWidth={strokeWidth}
         r={normalizedRadius}
-        cx="22" // 中心のX座標を変更
-        cy="22" // 中心のY座標を変更
+        cx="23" // 中心のX座標を変更
+        cy="23" // 中心のY座標を変更
         strokeDasharray={`${circumference} ${circumference}`} // 円周を指定
         strokeDashoffset={strokeDashoffset} // プログレスのオフセット
         style={{ transition: "stroke-dashoffset 0.5s ease-in-out" }} // アニメーション
