81c8f816359f3dc4790d6a8da07a3b64fbc7e85e
goshiEta <tamerika563@gmail.com>
2024-12-07 17:22:29 +0900

    ホバー時のアニメーションなどを作成

--- app/blog/components/pageTile.tsx
+++ app/blog/components/pageTile.tsx
@@ -13,12 +13,14 @@ export default function PageTile({ data }: { data: PageObjectResponse }) {
 
   return (
     <Link href={`/blog/${data.id}`} className={styles.article}>
-      <img
-        src={imgurl}
-        alt={`${title}の記事画像`}
-        width={300}
-        className={styles.article_img}
-      />
+      <div className={styles.img_box}>
+        <img
+          src={imgurl}
+          alt={`${title}の記事画像`}
+          width={300}
+          className={styles.article_img}
+        />
+      </div>
       <h2 className={styles.title}>{title}</h2>
       <p className={styles.published_date}>
         <span className="material-symbols-outlined">calendar_month</span>
