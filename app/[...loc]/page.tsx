14dfa98aa02f61581a8b05bd1db2bb69b462b205
goshiEta <tamerika563@gmail.com>
2024-12-08 17:27:51 +0900

    リダイレクトを設定

--- app/[...loc]/page.tsx
+++ app/[...loc]/page.tsx
@@ -1,19 +1,21 @@
 "use client";
 
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import { useEffect } from "react";
 
-export default async function Home() {
+export default function Redirect() {
   const router = useRouter();
-  const url = "https://kyouryoku.net";
+  const params = useParams();
+  const url = `https://kyouryoku.net/${(params.loc as any).join("/")}`;
+
   useEffect(() => {
     router.push(url);
   }, []);
 
   return (
     <div>
-      <h1>峡緑プレイは新しいドメインに移行しました</h1>
+      <h1>峡緑プレイは新しいドメインに移行しました。</h1>
       <p>新しいドメインにリダイレクトしています…</p>
       <Link href={url}>
         自動でリダイレクトされない場合はクリックしてください
